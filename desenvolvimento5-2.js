0b8ff24f5146e70f52baeabd63a259f8512f3d1a
Amanda <amanda.lleaoo@gmail.com>
2026-07-01 01:23:50 -0300

    d52

---
+++ desenvolvimento5-2.js
@@ -0,0 +1,8 @@
+function calcularOperacoesBasicas(num1, num2) {
+  console.log(`${num1} + ${num2} = ${num1 + num2}`);
+  console.log(`${num1} - ${num2} = ${num1 - num2}`);
+  console.log(`${num1} x ${num2} = ${num1 * num2}`);
+  console.log(`${num1} / ${num2} = ${num1 / num2}`);
+}
+
+calcularOperacoesBasicas(4, 5);
